e0c10931e0633f1acff9ff7bfe3f0887468e9ba2
Ghaith Magherbi <retr0creativegm@gmail.com>
2025-09-26 20:04:01 +0100

    Update main.tf
    add EC2 instance provisioning + SSH +SG

---
+++ terraform/main.tf
@@ -0,0 +1,48 @@
+resource "aws_key_pair" "default" {
+  key_name   = "dockerized-service-key"
+  public_key = var.ssh_public_key
+}
+
+variable "ssh_public_key" {
+  type = string
+}
+
+resource "aws_instance" "node_app" {
+  ami           = "ami-0360c520857e3138f" # Ubuntu 24.04
+  instance_type = "t3.micro"
+  key_name      = aws_key_pair.default.key_name
+
+  tags = {
+    Name = "dockerized-service-server"
+  }
+
+  security_groups = [aws_security_group.allow_http_ssh.name]
+}
+
+resource "aws_security_group" "allow_http_ssh" {
+  name        = "allow_http_ssh"
+  description = "Allow SSH and HTTP inbound traffic"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}

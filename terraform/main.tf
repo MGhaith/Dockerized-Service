cc57b3e575103f0b953d2fa74f9d4db9f5a03f4d
Ghaith Magherbi <retr0creativegm@gmail.com>
2025-09-28 22:57:53 +0100

    Update main.tf
    changed http ports to 3000 instead of 80

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,8 +33,8 @@ resource "aws_security_group" "allow_http_ssh" {
 
   ingress {
     description = "HTTP"
-    from_port   = 80
-    to_port     = 80
+    from_port   = 3000
+    to_port     = 3000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }

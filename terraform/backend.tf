4508761385b71d8aab1bd663097aad85072df007
Ghaith Magherbi <retr0creativegm@gmail.com>
2025-09-30 05:20:42 +0100

    Update backend.tf
    remove personal id

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,10 +2,10 @@ terraform {
   required_version = ">= 1.13.0"
 
   backend "s3" {
-    bucket         = "dockerized-service-terraform-state-075091538636"        # S3 bucket name (create once manually)
-    key            = "global/terraform.tfstate"                     # Path inside the bucket
-    region         = "us-east-1"                                    # Best Region for you
-    dynamodb_table = "dockerized-service-terraform-locks"                     # DynamoDB table for state locking
-    encrypt        = true                                           # Encrypt state at rest
+    bucket         = "dockerized-service-terraform-state-<Your AWS Account ID>"        # S3 bucket name (create once manually)
+    key            = "global/terraform.tfstate"                                        # Path inside the bucket
+    region         = "us-east-1"                                                       # Best Region for you
+    dynamodb_table = "dockerized-service-terraform-locks"                              # DynamoDB table for state locking
+    encrypt        = true                                                              # Encrypt state at rest
   }
 }
